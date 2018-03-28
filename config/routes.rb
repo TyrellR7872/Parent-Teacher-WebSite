f836cf42a01637e4880a9fa533a7e799d7e3d04d
Tyrell Roberts <troberts@colgate.edu>
2018-03-27 23:38:34 -0400

    Added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'user_accounts/new'
+
+  get 'user_accounts/create'
+
+  get 'user_accounts/update'
+
+  get 'user_account/new'
+
+  get 'user_account/create'
+
+  get 'user_account/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :user_accounts
   root "user_accounts#new"
