547ce01e35bbfab232fa1bb90cf7941b9c563125
Nam Nguyen <hnguyenvu@colgate.edu>
2018-03-31 18:58:24 -0400

    create view for new and edit, have not edited index and show

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -1,19 +1,31 @@
 class CalendarEventsController < ApplicationController
 
-    def index
-    end
+  def index
+    
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def edit
-    end
+  def create
+    # event = CalendarEvent.new(create_update_params)
+    # if event.save
+    #   flash[:success] = "New event \'#{event.title}\' created"
+    #   redirect_to calendar_events_path
+    # else
+    #   flash[:warning]= "Error creating new event"
+    #   redirect_to new_calendar_event_path(event)
+    # end
+  end
 
-    def new
-    end
+  def edit
+  end
 
-    def create_update_params
-        params.require(:calendar_event).permit(:title, :date, :time, :location, :description, :type, :contact_person, :is_approved)
-    end
+  def new
+  end
+
+  def create_update_params
+    params.require(:calendar_event).permit(:title, :date, :time, :location, :description, :type, :contact_person, :is_approved)
+  end
 
 end
