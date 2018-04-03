549afa34c80542ce226affeaf15d878f06f5cd8d
Nam Nguyen <hnguyenvu@colgate.edu>
2018-04-03 13:49:29 -0400

    truncate the event description in index, add show.html.haml

--- app/models/calendar_event.rb
+++ app/models/calendar_event.rb
@@ -1,3 +1,10 @@
 class CalendarEvent < ApplicationRecord
-  scope :event_between, ->(start_date, end_date){where("start_date_time >= ? AND end_date_time <= ?", start_date, end_date)}
+  scope :event_between, -> (start_date, end_date) {where("start_date_time >= ? AND end_date_time <= ?", start_date, end_date)}
+
+  scope :find_approved, -> (is_approved) {where("is_approved = ?", is_approved)}
+
+  scope :approved_confirmed, -> {find_approved(true)}
+
+  scope :approved_pending, -> {find_approved(false)}
+
 end
