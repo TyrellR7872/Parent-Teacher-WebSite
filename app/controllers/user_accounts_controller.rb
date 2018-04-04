bc78fce4b026a2a5371253b2f46217d77df94144
Tyrell Roberts <troberts@colgate.edu>
2018-04-04 04:28:21 -0400

    Added more User Account functionality and global partial header

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -5,14 +5,14 @@ class UserAccountsController < ApplicationController
 
   def create
     user = UserAccount.fill_user(UserAccount.new(create_update_params))
-    byebug
-    if !params[:pass_confirm].eql?user.password
-      flash[:error] = "Error updating profile"
-      redirect_to edit_user_account_path(user) and return
+    if !user.password_confirmation.eql?user.password
+      flash[:warning] = "Passwords are not equal"
+      redirect_to new_user_account_path and return
     end
     if user.save
       flash[:notice] = "User Account #{user.username} Successfully Created"
       session[:id] = user.id
+      session[:pass] = user.password
       redirect_to user_accounts_path and return
     else
       flash[:warning] = "User Account #{user.username} Creation Failed"
@@ -23,12 +23,17 @@ class UserAccountsController < ApplicationController
   def update
     user = UserAccount.find(session[:id])
     user.update(create_update_params)
+    user.password = session[:pass] if user.password == ""
+    if !user.valid?
+      flash[:warning] = "Please check your fields"
+      redirect_to edit_user_account_path(user) and return
+    end
     if user.save
       session[:id] = user.id
       flash[:notice] = "Profile \"#{user.username}\" updated"
       redirect_to user_accounts_path and return
     else
-      flash[:error] = "Error updating profile"
+      flash[:warning] = "Error updating profile"
       redirect_to edit_user_account_path(user) and return
     end
   end
@@ -39,13 +44,14 @@ class UserAccountsController < ApplicationController
 
   def index
     @user_account = UserAccount.find(session[:id])
-    byebug
+
   end
 
 
 private
   def create_update_params
-    params.require(:user_account).permit(:username,:password,:email,:childgrade,:childname,:homeaddress)
+    params.require(:user_account).permit(:username,:email,:childgrade,:childname,:homeaddress,:name, :password, :password_confirmation)
   end
+
 
 end
--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -1,4 +1,10 @@
 class UserAccount < ApplicationRecord
+  validates :username, presence: {message: "Must be given"}
+  validates :password, confirmation: true, on: create
+  validates :email, presence: {message: "Must be given"}
+
+
+
   def self.fill_user(user)
     user.childname = ""
     user.childgrade = 0
