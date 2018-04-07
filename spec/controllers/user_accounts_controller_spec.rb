7d5a435d25a0c1dcce09e2b9e04fbde7edd3348b
Asadius <ajamil@colgate.edu>
2018-04-06 20:47:49 -0400

    passing rspec tests-calendar coverage 100%

--- spec/controllers/user_accounts_controller_spec.rb
+++ spec/controllers/user_accounts_controller_spec.rb
@@ -11,15 +11,18 @@ RSpec.describe UserAccountsController, type: :controller do
 
   describe "GET #create" do
     it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+      get :create, params: {:user_account => {username: "TestUser", password: "testpass", email: "testuser@test.com", accounttype: "Parent", name: "Tester Name", childname: "Test Child", childgrade: 5, homeaddress: "123 Test Street"}}
+      expect(response).to have_http_status(:redirect)
     end
   end
 
   describe "GET #update" do
+    before(:example) do
+      ua = UserAccount.create!(username: "TestUser", password: "testpass", email: "testuser@test.com", accounttype: "Parent", name: "Tester Name", childname: "Test Child", childgrade: 5, homeaddress: "123 Test Street")
+    end
     it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
+      get :update, session: {:id => 1},  params: {:id => 1, :user_account => {username: "TestUser", password: "testpass", email: "testuser@test.com", accounttype: "Parent", name: "Tester Name", childname: "Test Child", childgrade: 5, homeaddress: "123 Test Street"}}
+      expect(response).to have_http_status(:redirect)
     end
   end
 
