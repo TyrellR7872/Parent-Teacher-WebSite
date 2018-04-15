47a67e901809ae3e9e38814948b7e25000611b2e
Nam Nguyen <hnguyenvu@colgate.edu>
2018-04-15 18:00:23 -0400

    delete unneccessary changes in pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  
+
     def show
     # Show the appropriate static pages from the /views folder, or
     # render file 404 error
@@ -9,5 +9,4 @@ class PagesController < ApplicationController
       render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found
     end
   end
-
 end
