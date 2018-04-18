966165175b24983d0344c4a5e87b15b07b733c36
Tyrell Roberts <troberts@colgate.edu>
2018-04-18 03:57:09 -0400

    100% coverage for specs

--- spec/controllers/user_accounts_controller_spec.rb
+++ spec/controllers/user_accounts_controller_spec.rb
@@ -4,10 +4,6 @@ RSpec.describe UserAccountsController, type: :controller do
 
 
   describe "check failure when entered input does not meet requirements set" do
-    it "returns an error when the username length < 5 and fail to create a user account" do
-      expect {
-        UserAccount.create!(email: "railsy@yahoo.com",childgrade: "10",childname: "Ruby Rails",homeaddress: "Ontario, Canada",name: "Ruby Ran", password: "VierDrei", password_confirmation: "VierDrei")}.to raise_exception ActiveRecord::RecordInvalid
-    end
     it "returns an error when no email address is entered and fails to create a user account" do
       expect {
           UserAccount.create!(email: "",childgrade: "11",childname: "Lamb Nose",homeaddress: "North Pole",name: "Red Nosed Rudolph", password: "SnowChristmasMustGo", password_confirmation: "SnowChristmasMustGo")}.to raise_exception ActiveRecord::RecordInvalid
@@ -18,36 +14,31 @@ RSpec.describe UserAccountsController, type: :controller do
     end
   end
 
-
-  # describe "GET #create" do
-  #   it "returns http success" do
-  #     get :create, params: {:user_account => { password: "testpass", email: "testuser@test.com", accounttype: "Parent", name: "Tester Name", childname: "Test Child", childgrade: 5, homeaddress: "123 Test Street"}}
-  #     expect(response).to have_http_status(:redirect)
-  #   end
-  # end
-  #
-  # describe "GET #update" do
-  #   before(:example) do
-  #     ua = UserAccount.create!(password: "testpass", email: "testuser@test.com", accounttype: "Parent", name: "Tester Name", childname: "Test Child", childgrade: 5, homeaddress: "123 Test Street")
-  #   end
-  #   it "returns http success" do
-  #     get :update, session: {:id => 1},  params: {:id => 1, :user_account => {password: "testpass", email: "testuser@test.com", accounttype: "Parent", name: "Tester Name", childname: "Test Child", childgrade: 5, homeaddress: "123 Test Street"}}
-  #     expect(response).to have_http_status(:redirect)
-  #   end
-  # end
-  describe "GET #show" do
-    it "redirects to index on a bad id" do
-      expect(UserAccount).to receive(:find).and_raise(ActiveRecord::RecordNotFound)
+  describe "GET #show not signed in" do
+    it "should redirect user if not signed in" do
+      expect(subject.current_user_account).to eq(nil)
       get :show, :params => {:id => 1}
       expect(response).to redirect_to(new_user_account_registration_path)
     end
+  end
+
+  describe "GET #show" do
+    login_user
+
+    it "should have a current_user" do
+      # note the fact that you should remove the "validate_session" parameter if this was a     scaffold-generated controller
+      expect(subject.current_user_account).to_not eq(nil)
+    end
+
 
     it "returns http success" do
-      user = UserAccount.new
-      expect(UserAccount).to receive(:find).with(eq(1).or eq("1")).and_return(user)
-      get :show, :params => {:id => "1"}
+      expect(UserAccount).to receive(:find).with(eq(subject.current_user_account.id)).and_return(subject.current_user_account)
+      get :show, :params => {:id => subject.current_user_account.id}
       expect(response).to have_http_status(:success)
     end
+
+
   end
+
 
 end
--- spec/models/user_account_spec.rb
+++ spec/models/user_account_spec.rb
@@ -5,20 +5,18 @@ RSpec.describe UserAccount, type: :model do
   describe "check attributes and methods" do
     it "should be able to create a UserAccount object which has correct methods" do
       ua = UserAccount.create!(password: "testpass", email: "testuser@test.com", accounttype: "Parent", name: "Tester Name", childname: "Test Child", childgrade: 5, homeaddress: "123 Test Street")
-      expect(ua).to respond_to :password
       expect(ua).to respond_to :email
       expect(ua).to respond_to :accounttype
       expect(ua).to respond_to :name
       expect(ua).to respond_to :childname
       expect(ua).to respond_to :childgrade
       expect(ua).to respond_to :homeaddress
-      expect(UserAccount).to respond_to :fill_user
+      expect(ua).to respond_to :validates_format_of
     end
   end
 
-
   before(:each) do
-    @attr = {
+    @attribute = {
       :name => "Example User",
       :email => "user@example.com",
       :password => "changeme",
@@ -27,74 +25,76 @@ RSpec.describe UserAccount, type: :model do
   end
 
   it "should create a new instance given a valid attribute" do
-    UserAccount.create!(@attr)
+    UserAccount.create!(@attribute)
   end
 
   it "should require an email address" do
-    no_email_user = UserAccount.new(@attr.merge(:email => ""))
-    no_email_user.should_not be_valid
+    empty_email_user = UserAccount.new(@attribute.merge(:email => ""))
+    expect(empty_email_user).not_to be_valid
   end
 
   it "should accept valid email addresses" do
-    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
-    addresses.each do |address|
-      valid_email_user = UserAccount.new(@attr.merge(:email => address))
-      valid_email_user.should be_valid
+    emails = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    emails.each do |email|
+      valid_email_user = UserAccount.new(@attribute.merge(:email => email))
+      expect(valid_email_user).to be_valid
     end
   end
 
   it "should reject invalid email addresses" do
-    addresses = %w[userfoo,com user_at_foo.org example.user@foo.]
+    addresses = ["userfoo,com","user_at_foo.org","example.user@foo."]
     addresses.each do |address|
-      invalid_email_user = UserAccount.new(@attr.merge(:email => address))
-      invalid_email_user.should_not be_valid
+      user =UserAccount.new(@attribute.merge(:email => address))
+      expect(user.valid?).to eq(false)
     end
   end
 
   it "should reject duplicate email addresses" do
-    UserAccount.create!(@attr)
-    user_with_duplicate_email = UserAccount.new(@attr)
-    user_with_duplicate_email.should_not be_valid
+    UserAccount.create!(@attribute)
+    user_with_duplicate_email = UserAccount.new(@attribute)
+    expect(user_with_duplicate_email).not_to be_valid
   end
 
   it "should reject email addresses identical up to case" do
-    upcased_email = @attr[:email].upcase
-    UserAccount.create!(@attr.merge(:email => upcased_email))
-    user_with_duplicate_email = UserAccount.new(@attr)
-    user_with_duplicate_email.should_not be_valid
+    upcased_email = @attribute[:email].upcase
+    UserAccount.create!(@attribute.merge(:email => upcased_email))
+    user_with_duplicate_email = UserAccount.new(@attribute)
+    expect(user_with_duplicate_email).not_to be_valid
   end
 
   describe "passwords" do
 
     before(:each) do
-      @user = UserAccount.new(@attr)
+      @user = UserAccount.new(@attribute)
     end
 
     it "should have a password attribute" do
-      @user.should respond_to(:password)
+      expect(@user).to respond_to(:password)
     end
 
     it "should have a password confirmation attribute" do
-      @user.should respond_to(:password_confirmation)
+      expect(@user).to respond_to(:password_confirmation)
+
     end
   end
 
   describe "password validations" do
 
     it "should require a password" do
-      UserAccount.new(@attr.merge(:password => "", :password_confirmation => "")).
-        should_not be_valid
+      user = UserAccount.new(@attribute.merge(:password => "", :password_confirmation => ""))
+      expect(user).not_to be_valid
     end
 
     it "should require a matching password confirmation" do
-      UserAccount.new(@attr.merge(:password_confirmation => "invalid")).
-        should_not be_valid
+      user = UserAccount.new(@attribute.merge(:password_confirmation => "invalid"))
+      expect(user).not_to be_valid
     end
 
     it "should reject short passwords" do
-      short = "a" * 5
-      hash = @attr.merge(:password => short, :password_confirmation => short)
-      UserAccount.new(hash).should_not be_valid
+      short_pass = "a" * 5
+      short_pass_user = @attribute.merge(:password => short_pass, :password_confirmation => short_pass)
+      user = UserAccount.new(short_pass_user)
+      expect(user).not_to be_valid
     end
 
   end
@@ -102,15 +102,15 @@ RSpec.describe UserAccount, type: :model do
   describe "password encryption" do
 
     before(:each) do
-      @user = UserAccount.create!(@attr)
+      @user = UserAccount.create!(@attribute)
     end
 
     it "should have an encrypted password attribute" do
-      @user.should respond_to(:encrypted_password)
+      expect(@user).to respond_to(:encrypted_password)
     end
 
     it "should set the encrypted password attribute" do
-      @user.encrypted_password.should_not be_blank
+      expect(@user.encrypted_password).not_to be_blank
     end
 
   end
