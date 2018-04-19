ea7348e2df16e693a584f9583018113c359f67cf
Priya Dhawka <pdhawka@colgate.edu>
2018-04-19 12:50:05 -0400

    merge?

--- db/schema.rb
+++ db/schema.rb
@@ -39,14 +39,6 @@ ActiveRecord::Schema.define(version: 20180415232304) do
   create_table "user_accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-<<<<<<< HEAD
-    t.string "accounttype"
-    t.string "name"
-    t.string "childname"
-    t.integer "childgrade"
-    t.string "homeaddress"
-=======
->>>>>>> master
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -57,14 +49,11 @@ ActiveRecord::Schema.define(version: 20180415232304) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
-=======
     t.string "accounttype", default: ""
     t.string "name", default: ""
     t.string "childname", default: ""
     t.integer "childgrade"
     t.string "homeaddress", default: ""
->>>>>>> master
     t.index ["email"], name: "index_user_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_user_accounts_on_reset_password_token", unique: true
   end
