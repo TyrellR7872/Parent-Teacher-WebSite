47b4441578f6748fbb8b3f309e8d77ba4ff96661
Tyrell Roberts <troberts@colgate.edu>
2018-04-24 22:39:42 -0400

    Removed unneeded comments from devise controllers

--- app/controllers/user_accounts/registrations_controller.rb
+++ app/controllers/user_accounts/registrations_controller.rb
@@ -1,28 +1,7 @@
-# frozen_string_literal: true
-
 class UserAccounts::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-  #
-  # # POST /resource
-  # def create
-  #   super
-  # end
-  #
-  # # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # # PUT /resource
-  # def update
-  #   super
-  # end
 
   # DELETE /resource
   def destroy
@@ -32,19 +11,6 @@ class UserAccounts::RegistrationsController < Devise::RegistrationsController
       redirect_to root_path
     end
   end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
 
   def configure_sign_up_params
     attrs = [:name,:childname,:childgrade, :homeaddress]
@@ -56,18 +22,5 @@ class UserAccounts::RegistrationsController < Devise::RegistrationsController
     attrs = [:name,:childname,:childgrade, :homeaddress]
     devise_parameter_sanitizer.permit(:account_update, keys: attrs)
   end
-  # private
-  #   def update_params
-  #     params.require(:user_account).permit(:email,:childgrade,:childname,:homeaddress,:name)
-  #   end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
