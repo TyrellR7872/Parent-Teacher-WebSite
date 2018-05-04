bb6ee3e80434c034db13a7a85f20fd17f7a27749
Nam Nguyen <hnguyenvu@colgate.edu>
2018-05-04 11:55:31 -0400

    fixed typo in children.js

--- app/assets/javascripts/children.js
+++ app/assets/javascripts/children.js
@@ -1,30 +1,19 @@
 "use strict";
 
 var children = (function() {
-    // make any additional "private" function or variables here
-    var increaseBtnOnclick = function(arg){
-          var data= arg.detail[0]
-          console.log(data)
-          jQuery("#htop").text(data["increase"])
-          jQuery("#htop").text(data["decrease"])
-          // document.getElementById("htop").value = Number(document.getElementById("htop").value)+ 1;
-      }
-      // var decreaseBtnOnclick = function(){
-      //     document.getElementById("htop").value = Number(document.getElementById("htop").value) - 1;
-      // }
+  // make any additional "private" function or variables here
+  var increaseBtnOnclick = function(arg){
+    var data= arg.detail[0];
+    console.log(data);
+    jQuery("#htop").text(data["increase"]);
+    jQuery("#htop").text(data["decrease"]);
+  };
 
-    };
-
-    return {
-        init: function() {
-            //
-            // do any initialization for javascript here
-            //
-            $("a[data-remote]").bind("ajax:success", increaseBtnOnclick);
-            // $("a[data-remote]").bind("ajax:success", decreaseBtnOnclick);
-
-        },
-    };
-
+  return {
+    init: function() {
+      // do any initialization for javascript here
+      $("a[data-remote]").bind("ajax:success", increaseBtnOnclick);
+    }
+  };
 })();
 $(children.init);
